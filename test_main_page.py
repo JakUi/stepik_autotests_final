58bf2a12a02ad1ba48d3670c0417bd6267a22800
JakUi <48787844+JakUi@users.noreply.github.com>
2020-06-02 17:02:35 +0300

    Инициализируем LoginPage в теле теста

--- test_main_page.py
+++ test_main_page.py
@@ -1,5 +1,21 @@
-def test_guest_can_go_to_login_page(browser):
+from pages.main_page import MainPage
+from pages.locators import MainPageLocators
+from pages.login_page import LoginPage
+
+def test_guest_should_see_login_link(browser):
     link = "http://selenium1py.pythonanywhere.com/"
-    browser.get(link)
-    login_link = browser.find_element_by_css_selector("#login_link")
-    login_link.click()
+    page = MainPage(browser, link) #инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
+    page.open()                    #открываем страницу
+    page.should_be_login_link()    #выполняем метод страницы - переходим на страницу логина
+ 
+def go_to_login_page(self):
+    link = self.browser.find_element(*MainPageLocators.LOGIN_LINK) #В методе, который осуществляет переход к странице логина, инициализируем новый объект Page и возвращаем его
+    link.click()
+
+def test_guest_can_go_to_login_page(browser):
+    link = "http://selenium1py.pythonanywhere.com"
+    page = MainPage(browser, link) #инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
+    page.open()                    #открываем страницу
+    page.go_to_login_page()        #выполняем метод страницы - переходим на страницу логина
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.should_be_login_page() #Выполняем функцию should_be_login_page в модуле login_page
