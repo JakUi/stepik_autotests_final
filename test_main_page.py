85173de8b235ec6266d917e96a5b5d0feba748f0
JakUi <48787844+JakUi@users.noreply.github.com>
2020-09-07 18:24:01 +0300

    Add files via upload

--- test_main_page.py
+++ test_main_page.py
@@ -3,20 +3,26 @@ from pages.locators import MainPageLocators
 from pages.login_page import LoginPage
 from pages.base_page import BasePage
 from pages.basket_page import BasketPage
+import pytest
 
-def test_guest_should_see_login_link(browser):
-    link = "http://selenium1py.pythonanywhere.com/en-gb"
-    page = MainPage(browser, link) #инициализируем класс MainPage, передаем в конструктор экземпляр драйвера и url адрес
-    page.open()                    #открываем страницу методом (функцией) open из класса MainPage
-    page.should_be_login_link()    #выполняем проверку should_be_login_link из класса BasePage (MainPage наследник его и наследует все его методы)
+@pytest.mark.login_guest
+class TestLoginFromMainPage():
+    def test_guest_can_go_to_login_page(self,browser):
+        link = "http://selenium1py.pythonanywhere.com/en-gb"
+        page = MainPage(browser, link) #инициализируем класс MainPage, передаем в конструктор экземпляр драйвера и url адрес
+        page.open()                    #открываем страницу методом (функцией) open из класса MainPage
+        page.go_to_login_page()        #выполняем метод страницы - переходим на страницу логина
+        login_page = LoginPage(browser, browser.current_url)
+        login_page.should_be_login_page() #выполняем проверку should_be_login_page из класса BasePage (MainPage наследник его и наследует все его методы)
 
-def test_guest_can_go_to_login_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/en-gb"
-    page = MainPage(browser, link) #инициализируем класс MainPage, передаем в конструктор экземпляр драйвера и url адрес
-    page.open()                    #открываем страницу методом (функцией) open из класса MainPage
-    page.go_to_login_page()        #выполняем метод страницы - переходим на страницу логина
-    login_page = LoginPage(browser, browser.current_url)
-    login_page.should_be_login_page() #выполняем проверку should_be_login_page из класса BasePage (MainPage наследник его и наследует все его методы)
+
+    def test_guest_should_see_login_link(self,browser):
+        link = "http://selenium1py.pythonanywhere.com/en-gb"
+        page = MainPage(browser, link) #инициализируем класс MainPage, передаем в конструктор экземпляр драйвера и url адрес
+        page.open()                    #открываем страницу методом (функцией) open из класса MainPage
+        page.should_be_login_link()    #выполняем проверку should_be_login_link из класса BasePage (MainPage наследник его и наследует все его методы)
+    
+
 
 def test_guest_cant_see_product_in_basket_opened_from_main_page(browser):
     link = "http://selenium1py.pythonanywhere.com/en-gb"
