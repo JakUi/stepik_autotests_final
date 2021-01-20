99b2ec7917da8c68a4395aa160a19fef1ca910f3
JakUi <48787844+JakUi@users.noreply.github.com>
2021-01-20 16:38:50 +0300

    Add files via upload

--- test_main_page.py
+++ test_main_page.py
@@ -5,30 +5,29 @@ from pages.base_page import BasePage
 from pages.basket_page import BasketPage
 import pytest
 
+
 @pytest.mark.login_guest
 class TestLoginFromMainPage():
+
     def test_guest_can_go_to_login_page(self,browser):
         link = "http://selenium1py.pythonanywhere.com/en-gb"
-        page = MainPage(browser, link) #инициализируем класс MainPage, передаем в конструктор экземпляр драйвера и url адрес
-        page.open()                    #открываем страницу методом (функцией) open из класса MainPage
-        page.go_to_login_page()        #выполняем метод страницы - переходим на страницу логина
+        page = MainPage(browser, link)
+        page.open()
+        page.go_to_login_page()
         login_page = LoginPage(browser, browser.current_url)
-        login_page.should_be_login_page() #выполняем проверку should_be_login_page из класса BasePage (MainPage наследник его и наследует все его методы)
-
+        login_page.should_be_login_page()
 
     def test_guest_should_see_login_link(self,browser):
         link = "http://selenium1py.pythonanywhere.com/en-gb"
-        page = MainPage(browser, link) #инициализируем класс MainPage, передаем в конструктор экземпляр драйвера и url адрес
-        page.open()                    #открываем страницу методом (функцией) open из класса MainPage
-        page.should_be_login_link()    #выполняем проверку should_be_login_link из класса BasePage (MainPage наследник его и наследует все его методы)
-    
+        page = MainPage(browser, link)
+        page.open()
+        page.should_be_login_link()
 
-
-def test_guest_cant_see_product_in_basket_opened_from_main_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/en-gb"
-    page = BasePage(browser, link) #инициализируем класс BasePage чтобы использовать его методы
-    page.open() #Открываем страницу (link передаём, поэтому откроется та страница, ссылку на которую мы передали)             
-    page.go_to_cart() #используем метод (выполняем функцию) go_to_cart (переход в корзину) класса BasePage
-    basket = BasketPage(browser, link) #инициализируем класс BasketPage чтобы использовать его методы
-    basket.basket_is_empty() #Проверяем, что корзина пуста
-    basket.basket_is_empty_message() #Проверяем, что есть сообщение о том, что корзина пуста
+    def test_guest_cant_see_product_in_basket_opened_from_main_page(browser):
+        link = "http://selenium1py.pythonanywhere.com/en-gb"
+        page = BasePage(browser, link)
+        page.open()
+        page.go_to_cart()
+        basket = BasketPage(browser, link)
+        basket.basket_is_empty()
+        basket.basket_is_empty_message()
--- test_product_page.py
+++ test_product_page.py
@@ -2,77 +2,81 @@ from pages.main_page import MainPage
 from pages.locators import MainPageLocators
 from pages.login_page import LoginPage
 from pages.product_page import ProductPage
+from pages.product_page import ProductPageLocators
 from pages.base_page import BasePage
 from pages.basket_page import BasketPage
+import pytest
+
 
 class TestUserAddToBasketFromProductPage():
-    
+
     @pytest.fixture(scope="function", autouse=True)
-    def setup(self):
-        MainPage = MainPage()
-        LoginPage= LoginPage()
-        BasePage = BasePage()
-        MainPage.go_to_login_page()
-        LoginPage.register_new_user()
-        BasePage.should_be_authorized_user()
+    def setup(self, browser):
+        link = "http://selenium1py.pythonanywhere.com/"
+        mainpage = MainPage(browser, link)
+        page = BasePage(browser, link)
+        loginpage = LoginPage(browser, link)
+        basepage = BasePage(browser, link)
+        page.open()
+        mainpage.go_to_login_page()
+        loginpage.register_new_user()
+        basepage.should_be_authorized_user()
 
     def test_user_cant_see_success_message(self, browser):
         link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209"
-        page = ProductPage(browser, link) #инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
-        page.open()                    #открываем страницу
-        page.should_not_be_success_message_is_not_element_present() #Проверяем, что нет сообщения об успехе с помощью is_not_element_present
-    
+        page = ProductPage(browser, link) # Инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
+        page.open() # Открываем страницу
+        page.should_not_be_success_message_is_not_element_present() # Проверяем, что нет сообщения об успехе с помощью is_not_element_present
+
     @pytest.mark.need_review
-    def test_user_can_add_product_to_basket(browser):
-        link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
-        link = f'{links}'
-        page = ProductPage(browser, link) #инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
-        page.open()                    #Открываем страницу
-        page.add_to_cart()             #Добавляем товар в корзину 
-        #получаем название товара добавленного в корзину
-        name = browser.find_elements(*ProductPageLocators.PRODUCT_ADDED_IN_CART)
-        name = name[0].text
-        #получаем стоиомсть товара добавленного в корзину
-        price = browser.find_element(*ProductPageLocators.PRICE_PRODUCT_ADDED_IN_CART).text
-        #проверяем, что название и стоимость товара добавляемого в корзину равны названию и стоимости товаров в корзине
-        assert name == page.get_name(), 'Название товара добавленного в корзину не соответствует названию товара добавляемого в корзину'
-        assert price  == page.get_price(), 'Цена товара добавленного в корзину не соответствует цене товара добавляемого в корзину'
-
-def test_guest_should_see_login_link(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
-    page = MainPage(browser, link) #инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
-    page.open()                    #открываем страницу
-    page.should_be_login_link()    #выполняем метод страницы - переходим на страницу логина
- 
-def test_guest_can_go_to_login_page(browser):
-    link = "http://selenium1py.pythonanywhere.com"
-    page = MainPage(browser, link) #инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
-    page.open()                    #открываем страницу
-    page.go_to_login_page()        #выполняем метод страницы - переходим на страницу логина
-    login_page = LoginPage(browser, browser.current_url)
-    login_page.should_be_login_page() #Выполняем функцию should_be_login_page в модуле login_page
-
-def test_guest_should_see_login_link_on_product_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
-    page = ProductPage(browser, link)
-    page.open()
-    page.should_be_login_link()
+    def test_user_can_add_product_to_basket(self, browser):
+        link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209"
+        page = ProductPage(browser, link)
+        page.open()
+        page.add_to_cart() # Добавляем товар в корзину
 
 @pytest.mark.need_review
 def test_guest_can_go_to_login_page_from_product_page(browser):
     link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
     page = ProductPage(browser, link)
     page.open()
-    page.go_to_login_page()        #выполняем метод страницы - переходим на страницу логина
+    page.go_to_login_page() # Выполняем метод страницы - переходим на страницу логина
     login_page = LoginPage(browser, browser.current_url)
-    login_page.should_be_login_page() #Выполняем функцию should_be_login_page в модуле login_page
+    login_page.should_be_login_page() # Выполняем функцию should_be_login_page в модуле login_page
 
 @pytest.mark.need_review
 def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
     link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/hacking-exposed-wireless_208/"
-    page = BasePage(browser, link) #инициализируем класс BasePage чтобы использовать его методы
-    page.open() #Открываем страницу (link передаём, поэтому откроется та страница, ссылку на которую мы передали)             
-    page.go_to_cart() #используем метод (выполняем функцию) go_to_cart (переход в корзину) класса BasePage
-    basket = BasketPage(browser, link) #инициализируем класс BasketPage чтобы использовать его методы
-    basket.basket_is_empty() #Проверяем, что корзина пуста, используя метод basket_is_empty класса BasketPage
-    basket.basket_is_empty_message() #Проверяем, что есть сообщение о том, что корзина пуста, используя метод basket_is_empty класса BasketPage
+    page = BasePage(browser, link) # Инициализируем класс BasePage чтобы использовать его методы
+    page.open() # Открываем страницу (link передаём, поэтому откроется та страница, ссылку на которую мы передали)
+    page.go_to_cart() # Используем метод (выполняем функцию) go_to_cart (переход в корзину) класса BasePage
+    basket = BasketPage(browser, link) # Инициализируем класс BasketPage чтобы использовать его методы
+    basket.basket_is_empty() # Проверяем, что корзина пуста, используя метод basket_is_empty класса BasketPage
+    basket.basket_is_empty_message() # Проверяем, что есть сообщение о том, что корзина пуста, используя метод basket_is_empty класса BasketPage
+
+@pytest.mark.need_review
+def test_guest_can_add_product_to_basket(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
+    page = ProductPage(browser, link)
+    page.open()
+    page.add_promo_to_cart(browser, link)
+
+def test_guest_should_see_login_link(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, link)
+    page.open()
+    page.should_be_login_link()
+
+def test_guest_can_go_to_login_page(browser):
+    link = "http://selenium1py.pythonanywhere.com"
+    page = MainPage(browser, link)
+    page.open()
+    page.go_to_login_page()
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.should_be_login_page()
+
+def test_guest_should_see_login_link_on_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.should_be_login_link()
